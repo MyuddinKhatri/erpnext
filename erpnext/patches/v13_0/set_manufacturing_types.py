475be0d891f6828cb55533b1696c22be3388bf61
Myuddin khatri <myuddin@digithinkit.com>
2020-08-21 11:33:01 +0530

    fixed message

--- erpnext/patches/v13_0/set_manufacturing_types.py
+++ erpnext/patches/v13_0/set_manufacturing_types.py
@@ -3,30 +3,11 @@ import frappe
 def execute():
 	for dt in ('BOM', 'Work Order', 'Stock Entry'):
 		frappe.reload_doctype(dt)
-
-	frappe.db.sql("""
-	UPDATE
-		`tabBOM`
-	SET
-		manufacturing_type = "Discrete"
-	WHERE
-		manufacturing_type IS NULL
-""")
-
-	frappe.db.sql("""
-	UPDATE
-		`tabWork Order`
-	SET
-		manufacturing_type = "Discrete"
-	WHERE
-		manufacturing_type IS NULL
-""")
-
-	frappe.db.sql("""
-	UPDATE
-		`tabStock Entry`
-	SET
-		manufacturing_type = "Discrete"
-	WHERE
-		manufacturing_type IS NULL
-""")
+		frappe.db.sql("""
+			UPDATE
+				`tab%s`
+			SET
+				manufacturing_type = "Discrete"
+			WHERE
+				manufacturing_type IS NULL
+		"""% (dt))
