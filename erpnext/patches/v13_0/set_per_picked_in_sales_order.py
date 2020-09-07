8af0d0ee7eefbc113dbab3a223922c0f4ff7fe92
Myuddin khatri <myuddin@digithinkit.com>
2020-09-07 10:03:08 +0530

    fixed changes

--- erpnext/patches/v13_0/set_per_picked_in_sales_order.py
+++ erpnext/patches/v13_0/set_per_picked_in_sales_order.py
@@ -3,10 +3,13 @@ import frappe
 def execute():
 	frappe.reload_doc('selling', 'doctype', 'sales_order', force=True)
 	frappe.reload_doc('stock', 'doctype', 'pick_list_item', force=True)
-	sales_orders = frappe.get_all("Sales Order")
+	sales_orders = frappe.get_all("Sales Order", filters={"docstatus": 1})
 
 	for order in sales_orders:
-		pick_list_items = frappe.get_all("Pick List Item", filters={"sales_order":order.name}, fields= ["sum(qty) as total_qty", "sum(picked_qty) as picked_qty"], group_by="item_code")
+		pick_list_items = frappe.get_all("Pick List Item",
+			filters={"docstatus": 1, "sales_order": order.name},
+			fields= ["sum(qty) as total_qty", "sum(picked_qty) as picked_qty"],
+			group_by="item_code")
 		picked_qty = sum(d['picked_qty'] for d in pick_list_items)
 		ordered_qty = sum(d['total_qty'] for d in pick_list_items)
 
