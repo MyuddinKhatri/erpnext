6cfbfca39b4a9a27088b3c8a782dd8e998f2b77e
Myuddin khatri <myuddin@digithinkit.com>
2020-09-14 16:09:41 +0530

    fix: sales order status

--- erpnext/stock/doctype/pick_list/pick_list.py
+++ erpnext/stock/doctype/pick_list/pick_list.py
@@ -9,7 +9,7 @@ from six import iteritems
 from frappe.model.document import Document
 from frappe import _
 from collections import OrderedDict
-from frappe.utils import floor, flt, today, cint
+from frappe.utils import floor, flt, today, cint, unique
 from frappe.model.mapper import get_mapped_doc, map_child_doc
 from erpnext.stock.get_item_details import get_conversion_factor
 from erpnext.selling.doctype.sales_order.sales_order import make_delivery_note as create_delivery_note_from_sales_order
@@ -186,10 +186,13 @@ class PickList(Document):
 					package_tag.save()
 
 	def set_per_picked(self):
-		picked_qty = sum(d.picked_qty for d in self.locations)
-		ordered_qty = sum(d.qty for d in self.locations)
-		per_picked = (picked_qty / ordered_qty) * 100
-		frappe.db.set_value("Sales Order", self.locations[0].sales_order, "per_picked", per_picked)
+		all_orders = [d.sales_order for d in self.locations]
+		for order in unique(all_orders):
+			picked_qty = sum(d.picked_qty for d in self.locations if d.sales_order == order)
+			ordered_qty = sum(d.qty for d in self.locations if d.sales_order == order)
+			per_picked = (picked_qty / ordered_qty) * 100
+
+			frappe.db.set_value("Sales Order", self.locations[0].sales_order, "per_picked", per_picked, update_modified=False)
 
 def validate_item_locations(pick_list):
 	if not pick_list.locations:
