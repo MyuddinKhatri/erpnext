5ccdb7c6d0534f78f535ee738c5155a4a6f286cf
Myuddin khatri <myuddin@digithinkit.com>
2020-12-02 14:09:39 +0530

    fix: changed indendation in package_tag.py

--- erpnext/compliance/doctype/package_tag/package_tag.py
+++ erpnext/compliance/doctype/package_tag/package_tag.py
@@ -54,6 +54,7 @@ def make_stock_reconciliation(item_code, batch_no, package_tag, qty, warehouse, 
 @frappe.whitelist()
 def make_waste_disposal(source_name, target_doc=None):
 	"""
+
 	Create Waste Disposal from a Package tag
 
 	Args:
